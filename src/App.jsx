21cc5919b4e9771e8455755d039599fca79ba7a1
Sharon Caphona Devanayagam <sharondeva197@gmail.com>
2025-03-23 13:33:15 +0530

    linking login and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Login from "./components/Login";
+import Home from "./components/Home";
 import Quiz from "./components/Quiz";
 import Result from "./components/Result";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Routes>
-      <Route path="/" element={<Quiz />} />
-      <Route path="/results" element={<Result />} />
+      {!isAuthenticated ? (
+        <Route path="/*" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+      ) : (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Quiz />} />
+          <Route path="/results" element={<Result />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </>
+      )}
     </Routes>
   );
 }
